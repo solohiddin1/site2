176ca529fadf0a171677f8518b1f5796afcb443a
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-12-25 13:24:46 +0500

    hosts

--- config/config.py
+++ config/config.py
@@ -1,3 +1,4 @@
+from pydantic import Field, model_validator
 from pydantic_settings import BaseSettings
 from typing import List
 from dotenv import load_dotenv
@@ -10,7 +11,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Settings(BaseSettings):
     SECRET_KEY: str
     DEBUG: bool
-    ALLOWED_HOSTS: List[str] = os.getenv("ALLOWED_HOSTS", "").split(",")
+    # ALLOWED_HOSTS: List[str] = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+    ALLOWED_HOSTS: List[str]
+    # ALLOWED_HOSTS: List[str] = Field(default=["127.0.0.1"])
+    # ALLOWED_HOSTS: List[str] = Field(default_factory=lambda: ["127.0.0.1"])
+
+    # @model_validator(mode="before")
+    # def split_hosts(cls, values):
+    #     if isinstance(values, str):
+    #         return [host.strip() for host in values.split(",") if host.strip()]
+    #     return values
+
     TELEGRAM_TOKEN: str
     CHAT_ID: str
 
